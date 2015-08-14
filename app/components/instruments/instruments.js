b3819d9e3ed6add681f5c2aa379ed2917836b6c9
Andrew Beale <ambeale@gmail.com>
2015-08-14 18:42:10 -0400

    Created drum machine interface

--- app/components/instruments/instruments.js
+++ app/components/instruments/instruments.js
@@ -2,13 +2,20 @@
 
 angular.module('myApp.instruments', [])
 
-.factory('Instrument', [function() {
+.factory('Instrument', ['BEATS_PER_INSTRUMENT', function(BEATS_PER_INSTRUMENT) {
     return function(display, filename) {
         this.display = display;
         this.filename = filename;
+        this.beats = [];
+        
+        for(var i = 0; i < BEATS_PER_INSTRUMENT; i++) {
+            this.beats.push(false);
+        }
         
         this.play = function() {
             new Audio('assets/audio/' + filename + '.wav').play();
         };
     };
-}]);
+}])
+
+.value('BEATS_PER_INSTRUMENT', 16);;
--- app/drum_view/drum_view.js
+++ app/drum_view/drum_view.js
@@ -9,7 +9,7 @@ angular.module('myApp.drum-machine', ['ngRoute'])
   });
 }])
 
-.controller('DrumCtrl', ['$scope', 'Instrument', function($scope, Instrument) {
+.controller('DrumCtrl', ['$scope', 'Instrument', 'BEATS_PER_INSTRUMENT', '$interval', function($scope, Instrument, BEATS_PER_INSTRUMENT, $interval) {
         
     var instruments = [];
     instruments.push(new Instrument('Kick', 'kick-acoustic01'));
@@ -29,4 +29,34 @@ angular.module('myApp.drum-machine', ['ngRoute'])
         return instruments;
     };
     
+    $scope.tempo = 120;
+    
+    var promise;
+    $scope.playIndex = -1;
+    
+    $scope.playBeats = function() {
+        var secondsPerBeat = (1 / ($scope.tempo / 60.0)) / 4;
+        
+        $scope.playIndex = 0;
+        promise = $interval(function() {
+            angular.forEach(instruments, function(instrument) {
+                if(instrument.beats[$scope.playIndex]) {
+                    instrument.play();
+                }
+            });
+            $scope.playIndex++;
+            if($scope.playIndex >= BEATS_PER_INSTRUMENT) {
+              $scope.playIndex = 0;  
+            }
+        }, secondsPerBeat * 1000);
+    };
+    
+    $scope.stopBeats = function() {
+       $interval.cancel(promise);
+       $scope.playIndex = -1;
+    };
+    
+    $scope.isPlaying = function() {
+        return $scope.playIndex != -1;
+    };
 }]);
